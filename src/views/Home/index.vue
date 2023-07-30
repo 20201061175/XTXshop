446b16ddc2d9a0ceeaf0942cb692f1f80283f689
tang <10948849@qq.com>
2023-07-31 02:22:34 +0800

    feat: 抽象GoodsItem

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -15,10 +15,4 @@ import homePanel from './components/homePanel.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
-  
-  <homePanel title="人气推荐" subTitle="人气推荐，好多商品">
-    <div>
-      我是人气推荐的插槽
-    </div>
-  </homePanel>
 </template>

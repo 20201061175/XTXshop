5d95f3ba1068905a169311a853f779f6ea3368ba
tang <10948849@qq.com>
2023-08-07 04:10:21 +0800

    feat: 头部购物车-计算价格/数量

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,5 +1,5 @@
 //购物车
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart',
@@ -20,8 +20,14 @@ export const useCartStore = defineStore('cart',
       cartList.value.splice(idx, 1)
     }
 
+    //计算属性
+    const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
+    const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+
     return {
       cartList,
+      allCount,
+      allPrice,
       addCart,
       delCart,
     }

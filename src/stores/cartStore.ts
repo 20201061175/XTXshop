c18e653c9ef42659c4b9dbb22003863ede3bbbc7
tang <10948849@qq.com>
2023-08-08 23:13:36 +0800

    feat: 本地购物车-购物车全选框

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -25,18 +25,25 @@ export const useCartStore = defineStore('cart',
       const item = cartList.value.find(item => item.skuId === skuId)
       item.selected = selected
     }
+    
+    const selectAll = (selected) => {
+      cartList.value.forEach(item => item.selected = selected)
+    }
 
     //计算属性
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+    const isAll = computed(() => cartList.value.every(item => item.selected))
 
     return {
       cartList,
       allCount,
       allPrice,
+      isAll,
       addCart,
       delCart,
       singleCheck,
+      selectAll,
     }
   },
   {
